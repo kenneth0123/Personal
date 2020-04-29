90bcb21845edfe5d913ebcfe0e516c2fcb0d5385
Kenneth Alvarez <kenbalv@gmail.com>
2020-04-29 12:37:09 -0700

    Updates

---
+++ precache-manifest.bf57b012722ae363b9309b9fbf318699.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd180e6838277c4c928d12dbaf9f9895",
+    "url": "/Personal/index.html"
+  },
+  {
+    "revision": "30d68bc770766ad73429",
+    "url": "/Personal/static/css/main.32550fb5.chunk.css"
+  },
+  {
+    "revision": "80d19a8aa21867cdea9b",
+    "url": "/Personal/static/js/2.15e3e1ba.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Personal/static/js/2.15e3e1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d68bc770766ad73429",
+    "url": "/Personal/static/js/main.833f83db.chunk.js"
+  },
+  {
+    "revision": "51840540ab7e9a7f6fe9",
+    "url": "/Personal/static/js/runtime-main.0df55423.js"
+  }
+]);
